ed8f783b7204a7c9845de586a0daea7f956a05a8
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2025-12-11 09:13:38 +0700

    qt5

--- db/dashboard.py
+++ db/dashboard.py
@@ -1,7 +1,7 @@
 import sys
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 from ui_untitled import Ui_MainWindow   # file .ui đã convert
 
